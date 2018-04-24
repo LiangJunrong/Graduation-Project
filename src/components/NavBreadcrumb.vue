e44cab4f33221b648cfe6709647ad355de24e35a
LiangJunrong <1741020489@qq.com>
2018-04-24 11:09:23 +0800

    Create NavBreadcrumb.vue

---
+++ src/components/NavBreadcrumb.vue
@@ -0,0 +1,12 @@
+<template>
+  <el-breadcrumb separator-class="el-icon-arrow-right" class="breadcrumb">
+    <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+    <slot></slot>
+  </el-breadcrumb>
+</template>
+<style>
+.breadcrumb{
+  margin-top: 10px;
+  margin-bottom: 10px;
+}
+</style>
